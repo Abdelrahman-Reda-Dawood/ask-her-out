63299622ecdfc8652b711d4124a4b98981563574
Abdelrahman-Reda-Dawood <116512972+Abdelrahman-Reda-Dawood@users.noreply.github.com>
2024-03-17 04:33:14 +0200

    added list of button messages

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,28 @@ import { useState } from "react";
 export default function Home() {
   const [sureButton, setSureButton] = useState(false);
   const [isSecondButtonHovered, setIsSecondButtonHovered] = useState(false);
+
+  const [buttonTextIndex, setButtonTextIndex] = useState(0);
+
+  const buttonTexts = [
+    "Sure",
+    "Absolutely",
+    "Of Course",
+    "I'd Love That",
+    "I Agree",
+  ];
+
+  const handleSecondButtonClick = () => {
+    setIsSecondButtonHovered(true);
+    setSureButton(false); // Reset the first button text
+    setButtonTextIndex((prevIndex) => (prevIndex + 1) % buttonTexts.length); // Cycle through the texts
+  };
+
   return (
     <main className="flex flex-col flex-wrap items-center min-h-screen p-24 text-white gap-14">
       <div className="flex flex-col items-center justify-between h-full gap-3">
         <Image
+          unoptimized
           className="aspect-square"
           src={`${
             sureButton
@@ -36,10 +54,11 @@ export default function Home() {
             setSureButton(true);
           }}
           className="p-2 font-semibold transition-colors bg-blue-400 rounded shadow-md sm:text-2xl hover:bg-blue-500 ring-1 ring-gray-100 duration-400 xs:text-md">
-          Sure
+          {buttonTexts[buttonTextIndex]}
         </button>
+
         <button
-          onClick={() => setSureButton(false)}
+          onClick={handleSecondButtonClick}
           onMouseEnter={() => {
             setIsSecondButtonHovered(true);
           }}
